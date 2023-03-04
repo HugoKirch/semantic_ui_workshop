96a7f201744982b1d030d950284ebac7770678cd
ankidets <hugo.kirch@epitech.eu>
2023-03-04 18:32:15 +0100

    correcting bug

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ejs = require('ejs');
 
-app.get('/', (req, res) => res.render('views/index'));
+app.set('view engine', 'ejs');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
